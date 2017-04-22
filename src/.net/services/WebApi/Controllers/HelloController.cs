7ef0626251736506f3521a7606c506a99b633405
NguyenTruongDev <nguyentruongdeveloper@gmail.com>
2017-04-22 14:48:55 -0700

    #5 initial project

---
+++ src/.net/services/WebApi/Controllers/HelloController.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    public class HelloController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage Test()
+        {
+            var obj = new { StatusCode = 200, data = new { HoTen = "Binh", Lop = "15HCB2" } };
+          
+            //string json = JsonConvert.SerializeObject(obj);
+
+
+
+            return Request.CreateResponse(HttpStatusCode.Created, obj);
+
+
+        }
+
+    }
+}
