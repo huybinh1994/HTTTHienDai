2ad677851653b1874ee3384b82f4a5566d00f614
vnkhong <xuanthai_dbk@yahoo.com>
2017-04-22 12:45:53 +0700

    Ket noi DB va demo json table master

---
+++ src/java/services/src/main/java/service/MasterServiceImpl.java
@@ -0,0 +1,30 @@
+package service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import dao.MasterDAO;
+import model.MasterDTO;
+
+@Service
+public class MasterServiceImpl implements MasterService{
+
+	MasterDAO master;
+	
+	public MasterDAO getMaster() {
+		return master;
+	}
+	@Autowired
+	public void setMaster(MasterDAO master) {
+		this.master = master;
+	}
+
+	@Override
+	public List<MasterDTO> getAll() {
+		// TODO Auto-generated method stub
+		return master.getAll();
+	}
+
+}
