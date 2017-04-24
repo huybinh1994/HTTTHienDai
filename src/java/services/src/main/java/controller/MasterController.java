3a8524555ef9823b56f8795a147db1d9b3627d39
vnkhong <xuanthai_dbk@yahoo.com>
2017-04-24 21:53:37 +0700

    #8 theo doi thu muc bat ky

--- src/java/services/src/main/java/controller/MasterController.java
+++ src/java/services/src/main/java/controller/MasterController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import java.nio.file.WatchService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -11,25 +13,43 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.google.gson.Gson;
 
 import service.MasterService;
+import service.WatchingService;
 
 @Controller
 @Transactional
 @EnableTransactionManagement
 public class MasterController {
+	@Autowired
+	WatchingService watchService;
 
-MasterService masterService;
+	public WatchingService getWatchService() {
+		return watchService;
+	}
+
 	
+	public void setWatchService(WatchingService watchService) {
+		this.watchService = watchService;
+	}
+
+	MasterService masterService;
+
 	public MasterService getMasterService() {
 		return masterService;
 	}
+
 	@Autowired
 	public void setMasterService(MasterService masterService) {
 		this.masterService = masterService;
 	}
-	
+
 	@RequestMapping(value = "/listmaster", method = RequestMethod.GET)
-	public @ResponseBody String getlistMaster() {		
+	public @ResponseBody String getlistMaster() {
 		String json = new Gson().toJson(masterService.getAll());
 		return json;
+	}
+
+	@RequestMapping(value = "/theodoi", method = RequestMethod.GET)
+	public  void theodoi() {
+		watchService.watching();
 	}
 }
