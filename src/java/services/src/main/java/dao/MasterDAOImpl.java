a9ed90a349e3a032eb4323c2cb3c1fb0569e02ea
vnkhong <xuanthai_dbk@yahoo.com>
2017-05-17 20:38:31 +0700

    #11 auto scan project

--- src/java/services/src/main/java/dao/MasterDAOImpl.java
+++ src/java/services/src/main/java/dao/MasterDAOImpl.java
@@ -7,11 +7,13 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import model.MasterDTO;
 
-@Transactional
+//@Transactional
+@Repository
 public class MasterDAOImpl implements MasterDAO {
 
 	private SessionFactory sessionFactory;
