66eb8b1b664973c3c7e2602d99a7cf81d3f08890
vnkhong <xuanthai_dbk@yahoo.com>
2017-06-11 11:43:50 +0700

    #12 add dto report

---
+++ src/java/services/src/main/java/model/MasterReportCardtype.java
@@ -0,0 +1,62 @@
+package model;
+
+public class MasterReportCardtype {
+	private int id;
+	private String mearchant_name;
+	private String merchant_code;	
+	private String card_type;
+	private String total_quantity_sale;
+	private String total_amout_sale;
+	private String total_quantity_return;
+	private String total_amout_return;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getMearchant_name() {
+		return mearchant_name;
+	}
+	public void setMearchant_name(String mearchant_name) {
+		this.mearchant_name = mearchant_name;
+	}
+	public String getMerchant_code() {
+		return merchant_code;
+	}
+	public void setMerchant_code(String merchant_code) {
+		this.merchant_code = merchant_code;
+	}
+	public String getCard_type() {
+		return card_type;
+	}
+	public void setCard_type(String card_type) {
+		this.card_type = card_type;
+	}
+	public String getTotal_quantity_sale() {
+		return total_quantity_sale;
+	}
+	public void setTotal_quantity_sale(String total_quantity_sale) {
+		this.total_quantity_sale = total_quantity_sale;
+	}
+	public String getTotal_amout_sale() {
+		return total_amout_sale;
+	}
+	public void setTotal_amout_sale(String total_amout_sale) {
+		this.total_amout_sale = total_amout_sale;
+	}
+	public String getTotal_quantity_return() {
+		return total_quantity_return;
+	}
+	public void setTotal_quantity_return(String total_quantity_return) {
+		this.total_quantity_return = total_quantity_return;
+	}
+	public String getTotal_amout_return() {
+		return total_amout_return;
+	}
+	public void setTotal_amout_return(String total_amout_return) {
+		this.total_amout_return = total_amout_return;
+	}
+	
+
+}
